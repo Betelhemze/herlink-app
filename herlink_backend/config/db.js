4b6efe4cfae3519dff55fa5af4f652a1fb7bd854
betelhemze <betelhemwondale@gmail.com>
2026-01-05 02:50:29 +0300

    connected with render db

--- herlink_backend/config/db.js
+++ herlink_backend/config/db.js
@@ -6,10 +6,9 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: process.env.DATABASE_URL?.includes("render.com")
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 
